07e78ff38192d7e693131b414458e7b47e1cb224
Oliver Wooding <oliverwooding@icloud.com>
2024-07-28 23:13:58 +0200

    got far enough into prototype3 to find out why it's useless. yippee

--- prototype3/src/token.rs
+++ prototype3/src/token.rs
@@ -1,9 +1,11 @@
-use byteyarn::Yarn;
+//! The [`Token`] type and its [`Logos`] lexer implementation.
+
+use byteyarn::YarnBox;
 use logos::{Lexer, Logos};
 
 #[derive(Debug, Clone, Logos)]
 #[logos(skip r"[ \t\r\f\n]+")]
-pub enum Token {
+pub enum Token<'src> {
     // BRACKETS
     #[token("(")]
     LeftParen,
@@ -148,87 +150,43 @@ pub enum Token {
 
     // LITERALS
     #[regex(r#""(\\"|[^"\r])*""#, string_literal)]
-    StringLiteral(Yarn),
+    StringLiteral(YarnBox<'src, str>),
     #[regex(r#"[0-9]+\.[0-9]+(f32|f64)?"#, str_to_yarn)]
-    FloatLiteral(Yarn),
+    FloatLiteral(YarnBox<'src, str>),
     #[regex(r#"0b[01]+((u|i)(8|16|32|64))?"#, str_to_yarn)]
-    BinLiteral(Yarn),
+    BinLiteral(YarnBox<'src, str>),
     #[regex(r#"0o[0-7]+((u|i)(8|16|32|64))?"#, str_to_yarn)]
-    OctLiteral(Yarn),
+    OctLiteral(YarnBox<'src, str>),
     #[regex(r#"[0-9]+(((u|i)(8|16|32|64))|f32|f64)?"#, str_to_yarn)]
-    DecLiteral(Yarn),
+    DecLiteral(YarnBox<'src, str>),
     #[regex(r#"0(x|X)[0-9a-fA-F]+((u|i)(8|16|32|64))?"#, str_to_yarn)]
-    HexLiteral(Yarn),
+    HexLiteral(YarnBox<'src, str>),
 
     // IDENTIFIERS
     #[regex(r#"\.(_+[a-zA-Z0-9]|[a-zA-Z])[_a-zA-Z0-9]*"#, str_to_yarn)]
-    Projection(Yarn),
+    Projection(YarnBox<'src, str>),
     #[regex(r#"(_+[a-zA-Z0-9]|[a-zA-Z])[_a-zA-Z0-9]*"#, str_to_yarn)]
-    Ident(Yarn),
+    Ident(YarnBox<'src, str>),
     #[regex(
         r#"(_+[a-zA-Z0-9]|[a-zA-Z])[_a-zA-Z0-9]*(::(_+[a-zA-Z0-9]|[a-zA-Z])[_a-zA-Z0-9]*)+"#,
         str_to_yarn
     )]
-    Name(Yarn),
+    Name(YarnBox<'src, str>),
 }
 
-fn string_literal(lexer: &mut Lexer<Token>) -> Option<Yarn> {
+fn string_literal<'src>(lexer: &mut Lexer<'src, Token<'src>>) -> Option<YarnBox<'src, str>> {
     let slice = lexer.slice();
     let content = slice.strip_prefix("\"")?.strip_suffix("\"")?;
-    Some(Yarn::copy(content))
+    Some(YarnBox::new(content))
 }
 
-fn str_to_yarn(lexer: &mut Lexer<Token>) -> Yarn {
-    Yarn::copy(lexer.slice())
-}
-
-/// A qualified name.
-#[derive(Debug, Clone)]
-pub struct Name(Box<[Yarn]>);
-
-impl std::fmt::Display for Name {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let (head, tail) = self
-            .as_slice()
-            .split_first()
-            .expect("a Name contains at least one element");
-
-        write!(f, "{}", head.as_str())?;
-
-        for elem in tail {
-            write!(f, "::{}", elem.as_str())?;
-        }
-
-        Ok(())
-    }
-}
-
-impl Name {
-    pub fn new(head: Yarn, tail: impl IntoIterator<Item = Yarn>) -> Self {
-        Name(std::iter::once(head).chain(tail).collect::<Box<[_]>>())
-    }
-
-    /// Returns the components of the name in order,
-    /// guaranteeing at least one element.
-    pub fn as_slice(&self) -> &[Yarn] {
-        &self.0
-    }
+fn str_to_yarn<'src>(lexer: &mut Lexer<'src, Token<'src>>) -> YarnBox<'src, str> {
+    YarnBox::new(lexer.slice())
 }
 
 #[cfg(test)]
 mod tests {
-    use byteyarn::yarn;
-
     use super::*;
-
-    #[test]
-    fn name_display_impl_is_correct() {
-        let foo = Name::new(yarn!("foo"), std::iter::empty());
-        assert_eq!(format!("{}", foo), "foo");
-
-        let foo_bar_baz = Name::new(yarn!("foo"), vec![yarn!("bar"), yarn!("baz")]);
-        assert_eq!(format!("{}", foo_bar_baz), "foo::bar::baz");
-    }
 
     #[test]
     fn lexer_testing() {
@@ -245,6 +203,8 @@ mod tests {
                 Ok(T),
                 Err(E),
             }
+
+            def deep_inner(x: _) = x.y.z.w . foo
         "#;
         let lexer = Token::lexer(source);
         let tokens = lexer.into_iter().map(Result::unwrap).collect::<Vec<_>>();
