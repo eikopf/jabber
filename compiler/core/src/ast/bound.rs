eca409fa379a4273d59eecadd731d5ea82f07cee
Oliver Wooding <oliverwooding@icloud.com>
2024-11-19 12:51:40 +0000

    parameterised types over N = Name in env::ast::bound for better error tracking

--- compiler/core/src/ast/bound.rs
+++ compiler/core/src/ast/bound.rs
@@ -10,124 +10,124 @@ use crate::{
 // DECLARATIONS
 
 #[derive(Debug, Clone)]
-pub enum Type {
-    Alias(TypeAlias),
-    Adt(Adt),
-    Extern(ExternType),
+pub enum Type<N = Name> {
+    Alias(TypeAlias<N>),
+    Adt(Adt<N>),
+    Extern(ExternType<N>),
 }
 
 #[derive(Debug, Clone)]
-pub struct TypeAlias {
-    pub name: Spanned<Ident>,
-    pub params: SpanSeq<Ident>,
-    pub ty: Spanned<Ty>,
+pub struct TypeAlias<N = Name> {
+    pub name: Spanned<N>,
+    pub params: SpanSeq<N>,
+    pub ty: Spanned<Ty<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct Adt {
-    pub name: Spanned<Ident>,
+pub struct Adt<N = Name> {
+    pub name: Spanned<N>,
     pub opacity: Option<Span>,
-    pub params: SpanSeq<Ident>,
-    pub constructors: SpanSeq<TyConstr>,
+    pub params: SpanSeq<N>,
+    pub constructors: SpanSeq<TyConstr<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct ExternType {
-    pub name: Spanned<Ident>,
-    pub params: SpanSeq<Ident>,
+pub struct ExternType<N = Name> {
+    pub name: Spanned<N>,
+    pub params: SpanSeq<N>,
 }
 
 #[derive(Debug, Clone)]
-pub enum Term {
-    Fn(Fn),
-    ExternFn(ExternFn),
-    Const(Const),
+pub enum Term<N = Name> {
+    Fn(Fn<N>),
+    ExternFn(ExternFn<N>),
+    Const(Const<N>),
 }
 
 #[derive(Debug, Clone)]
-pub struct Fn {
-    pub name: Spanned<Ident>,
-    pub params: SpanSeq<Parameter>,
-    pub return_ty: Spanned<Ty>,
-    pub body: SpanBox<Expr>,
+pub struct Fn<N = Name> {
+    pub name: Spanned<N>,
+    pub params: SpanSeq<Parameter<N>>,
+    pub return_ty: Spanned<Ty<N>>,
+    pub body: SpanBox<Expr<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct ExternFn {
-    pub name: Spanned<Ident>,
-    pub params: SpanSeq<Parameter>,
-    pub return_ty: Spanned<Ty>,
+pub struct ExternFn<N = Name> {
+    pub name: Spanned<N>,
+    pub params: SpanSeq<Parameter<N>>,
+    pub return_ty: Spanned<Ty<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct Const {
-    pub name: Spanned<Ident>,
-    pub ty: Spanned<Ty>,
-    pub value: Spanned<Expr>,
+pub struct Const<N = Name> {
+    pub name: Spanned<N>,
+    pub ty: Spanned<Ty<N>>,
+    pub value: Spanned<Expr<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct TyConstr {
-    pub name: Spanned<Ident>,
-    pub ty: Uid,
-    pub payload: Spanned<TyConstrPayload>,
+pub struct TyConstr<N = Name> {
+    pub name: Spanned<N>,
+    pub ty: env::TypeId,
+    pub payload: Spanned<TyConstrPayload<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub enum TyConstrPayload {
+pub enum TyConstrPayload<N = Name> {
     Unit,
-    Tuple(SpanSeq<Ty>),
-    Record(SpanSeq<RecordField>),
+    Tuple(SpanSeq<Ty<N>>),
+    Record(SpanSeq<RecordField<N>>),
 }
 
 #[derive(Debug, Clone)]
-pub struct RecordField {
+pub struct RecordField<N = Name> {
     pub mutability: Option<Span>,
-    pub name: Spanned<Ident>,
-    pub ty: Spanned<Ty>,
+    pub name: Spanned<N>,
+    pub ty: Spanned<Ty<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct Parameter {
-    pub pattern: Spanned<Pattern>,
-    pub ty: Option<Spanned<Ty>>,
+pub struct Parameter<N = Name> {
+    pub pattern: Spanned<Pattern<N>>,
+    pub ty: Option<Spanned<Ty<N>>>,
 }
 
 // EXPRESSIONS
 
 #[derive(Debug, Clone)]
-pub enum Expr {
+pub enum Expr<N = Name> {
     Term(env::TermId),
-    Ident(Ident),
+    Ident(N),
     Literal(LiteralExpr),
     List(SpanSeq<Self>),
     Tuple(SpanSeq<Self>),
-    Block(Block),
+    Block(Block<N>),
     UnitConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
+        name: Spanned<N>,
     },
     TupleConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
+        name: Spanned<N>,
         args: SpanSeq<Self>,
     },
     RecordConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
-        fields: SpanSeq<RecordExprField>,
+        name: Spanned<N>,
+        fields: SpanSeq<RecordExprField<N>>,
         base: Option<SpanBox<Self>>,
     },
     RecordField {
         item: SpanBox<Self>,
-        field: Spanned<Ident>,
+        field: Spanned<N>,
     },
     TupleField {
         item: SpanBox<Self>,
         field: Spanned<Symbol>,
     },
     Lambda {
-        params: SpanSeq<Parameter>,
+        params: SpanSeq<Parameter<N>>,
         body: SpanBox<Self>,
     },
     Call {
@@ -136,12 +136,12 @@ pub enum Expr {
     },
     Match {
         scrutinee: SpanBox<Self>,
-        arms: SpanSeq<MatchArm>,
+        arms: SpanSeq<MatchArm<N>>,
     },
     IfElse {
         condition: SpanBox<Self>,
-        consequence: SpanBox<Block>,
-        alternative: SpanBox<Block>,
+        consequence: SpanBox<Block<N>>,
+        alternative: Option<SpanBox<Block<N>>>,
     },
 }
 
@@ -156,41 +156,40 @@ pub enum LiteralExpr {
 }
 
 #[derive(Debug, Clone)]
-pub struct RecordExprField {
-    pub field: Spanned<Ident>,
-    pub value: Spanned<Expr>,
+pub struct RecordExprField<N = Name> {
+    pub field: Spanned<N>,
+    pub value: Spanned<Expr<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct MatchArm {
-    pub pattern: Spanned<Pattern>,
-    pub guard: Option<Spanned<Expr>>,
-    pub body: Spanned<Expr>,
+pub struct MatchArm<N = Name> {
+    pub pattern: Spanned<Pattern<N>>,
+    pub body: Spanned<Expr<N>>,
 }
 
 #[derive(Debug, Clone)]
-pub struct Block {
-    pub statements: SpanSeq<Stmt>,
-    pub return_expr: Option<SpanBox<Expr>>,
+pub struct Block<N = Name> {
+    pub statements: SpanSeq<Stmt<N>>,
+    pub return_expr: Option<SpanBox<Expr<N>>>,
 }
 
 #[derive(Debug, Clone)]
-pub enum Stmt {
+pub enum Stmt<N = Name> {
     Empty,
-    Expr(Expr),
+    Expr(Expr<N>),
     Let {
-        pattern: Spanned<Pattern>,
-        ty: Option<Spanned<Ty>>,
-        value: Spanned<Expr>,
+        pattern: Spanned<Pattern<N>>,
+        ty: Option<Spanned<Ty<N>>>,
+        value: Spanned<Expr<N>>,
     },
 }
 
 // PATTERNS
 
 #[derive(Debug, Clone)]
-pub enum Pattern {
+pub enum Pattern<N = Name> {
     Wildcard,
-    Var(Ident),
+    Var(N),
     Literal(LiteralExpr),
     Tuple(SpanSeq<Self>),
     List(SpanSeq<Self>),
@@ -200,31 +199,31 @@ pub enum Pattern {
     },
     UnitConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
+        name: Spanned<N>,
     },
     TupleConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
+        name: Spanned<N>,
         elems: SpanSeq<Self>,
     },
     RecordConstr {
         ty: env::TypeId,
-        name: Spanned<Ident>,
-        fields: SpanSeq<RecordPatternField>,
+        name: Spanned<N>,
+        fields: SpanSeq<RecordPatternField<N>>,
         rest: Option<Span>,
     },
 }
 
 #[derive(Debug, Clone)]
-pub struct RecordPatternField {
-    pub field: Spanned<Ident>,
-    pub pattern: Spanned<Pattern>,
+pub struct RecordPatternField<N = Name> {
+    pub field: Spanned<N>,
+    pub pattern: Spanned<Pattern<N>>,
 }
 
 // TYPES
 
 #[derive(Debug, Clone)]
-pub enum Ty {
+pub enum Ty<N = Name> {
     Infer,
     Never,
     Unit,
@@ -232,9 +231,10 @@ pub enum Ty {
     String,
     Int,
     Float,
-    Var(Ident),
-    Adt {
-        name: Spanned<env::TypeId>,
+    Var(N),
+    Named {
+        ty: env::TypeId,
+        name: Spanned<Symbol>,
         args: SpanSeq<Self>,
     },
     Fn {
@@ -246,7 +246,13 @@ pub enum Ty {
 // NAMES
 
 #[derive(Debug, Clone, Copy)]
-pub struct Ident {
-    pub symbol: Symbol,
-    pub id: Uid,
+pub enum Name {
+    Local {
+        ident: Spanned<Symbol>,
+        id: Uid,
+    },
+    Global {
+        ident: Spanned<Symbol>,
+        id: env::Res,
+    },
 }
