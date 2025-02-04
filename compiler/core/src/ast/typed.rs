f96546e90432bad64bbcafbd1aa0a2717791f7a5
Oliver Wooding <oliverwooding@icloud.com>
2025-02-04 16:51:40 +0000

    basic structure for typed AST

--- compiler/core/src/ast/typed.rs
+++ compiler/core/src/ast/typed.rs
@@ -1,1 +1,207 @@
 //! Typed ASTs derived from [bound ASTs][`super::bound`].
+//!
+//! # Terminology
+//!
+//! The words _term_ and _type_ are used as in the previous ASTs, where in
+//! particular a _type_ is a particular kind of declaration rather than a
+//! type in the mathematical sense.
+//!
+//! We instead use the shortened _ty_ (hence [`Ty`]) to refer to the proper
+//! types to be inferred and checked by the compiler. These types do not
+//! necessarily reflect any kind of syntactic information in the source code,
+//! but are instead purely logical constructs used to reason about the code.
+
+use std::{collections::HashMap, sync::Arc};
+
+use crate::{
+    env::{Res, TypeId},
+    span::{Span, SpanBox, SpanSeq, Spanned},
+    symbol::Symbol,
+    unique::Uid,
+};
+
+pub use super::bound::{
+    BindingId, Bound, CallExprKind, GlobalBinding, LiteralExpr, LocalBinding,
+    Name, NameContent, Path, PathBinding, Pattern, ResAttr, Ty as TyAst,
+    TyConstr, TyConstrPayload,
+};
+
+// DECLARATIONS
+
+/// A type declaration.
+///
+/// Based on the value of `kind`, this struct will represent either an ADT,
+/// a type alias, or an external type declaration.
+pub struct Type<N = Bound, V = Uid, A = ResAttr> {
+    pub attrs: SpanSeq<A>,
+    pub name: Name<Res>,
+    pub params: Box<[LocalBinding]>,
+    pub ty: Ty<TypeId, V>,
+    pub kind: Spanned<TypeKind<N>>,
+}
+
+/// The kind of a [`Type`] together with any specific elements belonging to
+/// each kind.
+pub enum TypeKind<N = Bound> {
+    Alias {
+        rhs: Spanned<TyAst<N>>,
+    },
+    Adt {
+        opacity: Option<Span>,
+        constructors: HashMap<Symbol, Spanned<TyConstr<N>>>,
+    },
+    Extern,
+}
+
+pub struct Term<N = Bound, V = Uid, A = ResAttr> {
+    pub attrs: SpanSeq<A>,
+    pub name: Name<Res>,
+    pub ty: Ty<TypeId, V>,
+    pub kind: Spanned<TermKind<N>>,
+}
+
+pub enum TermKind<N = Bound> {
+    Fn(N),
+    ExternFn,
+    Const,
+}
+
+// EXPRESSIONS
+
+#[derive(Debug, Clone)]
+pub enum Expr<N = Bound, V = Uid> {
+    Name(Typed<N, N, V>),
+    Literal(LiteralExpr),
+    List(TySpanSeq<Self, N, V>),
+    Tuple(TySpanSeq<Self, N, V>),
+    LetIn {
+        lhs: Option<LetStmtLhs<N>>,
+        rhs: TySpanBox<Self, N, V>,
+        body: TySpanBox<Self, N, V>,
+    },
+    RecordConstr {
+        name: N,
+        fields: SpanSeq<RecordExprField<N, V>>,
+        base: Option<TySpanBox<Self, N, V>>,
+    },
+    RecordField {
+        item: TySpanBox<Self, N, V>,
+        field: Spanned<Symbol>,
+    },
+    TupleField {
+        item: TySpanBox<Self, N, V>,
+        field: Spanned<u32>,
+    },
+    Lambda {
+        params: TySpanSeq<Parameter<N>, N, V>,
+        body: TySpanBox<Self, N, V>,
+    },
+    Call {
+        callee: TySpanBox<Self, N, V>,
+        args: TySpanSeq<Self, N, V>,
+        kind: CallExprKind,
+    },
+    Builtin {
+        operator: Spanned<BuiltinOperator>,
+        lhs: TySpanBox<Self, N, V>,
+        rhs: TySpanBox<Self, N, V>,
+    },
+    Match {
+        scrutinee: TySpanBox<Self, N, V>,
+        arms: SpanSeq<MatchArm<N, V>>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct RecordExprField<N = Bound, V = Uid> {
+    pub field: Spanned<Symbol>,
+    pub value: Spanned<Typed<Expr<N, V>, N, V>>,
+}
+
+/// The lefthand side of a `let` stmt, consisting of a pattern with an
+/// optional type annotation.
+#[derive(Debug, Clone)]
+pub struct LetStmtLhs<N = Bound> {
+    pub pattern: Spanned<Pattern<N>>,
+    pub ty_ast: Option<SpanBox<TyAst<N>>>,
+}
+
+/// A builtin operator.
+#[derive(Debug, Clone, Copy)]
+pub enum BuiltinOperator {
+    LazyAnd,
+    LazyOr,
+    Mutate,
+}
+
+#[derive(Debug, Clone)]
+pub struct MatchArm<N = Bound, V = Uid> {
+    pub pattern: Spanned<Pattern<N>>,
+    pub body: Spanned<Typed<Expr<N, V>, N, V>>,
+}
+
+// PATTERNS
+
+#[derive(Debug, Clone)]
+pub struct Parameter<N = Bound> {
+    pub pattern: Spanned<Pattern<N>>,
+    pub ty_ast: Option<Spanned<TyAst<N>>>,
+}
+
+// PROPER TYPES
+
+pub type TySpanSeq<T, N, V> = SpanSeq<Typed<T, N, V>>;
+pub type TySpanBox<T, N, V> = SpanBox<Typed<T, N, V>>;
+
+#[derive(Debug, Clone)]
+pub struct Typed<T, N, V> {
+    pub item: T,
+    pub ty: Arc<Ty<N, V>>,
+}
+
+impl<T, N, V> std::ops::Deref for Typed<T, N, V> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.item
+    }
+}
+
+/// A prenex type consisting of universally quantified type variables and
+/// a subject matrix.
+#[derive(Debug, Clone)]
+pub struct Ty<N, V> {
+    pub vars: Box<[V]>,
+    pub matrix: TyMatrix<N, V>,
+}
+
+/// The *matrix* of a type.
+///
+/// In a [`Ty`], the `matrix` is the portion of the type which does not
+/// contain any universal quantifiers.
+#[derive(Debug, Clone)]
+pub enum TyMatrix<N, V> {
+    Prim(PrimTy),
+    Var(V),
+    Tuple(Box<[Self]>),
+    Adt {
+        name: N,
+        args: Box<[Self]>,
+    },
+    Fn {
+        domain: Box<[Self]>,
+        codomain: Box<Self>,
+    },
+}
+
+/// A primitive type.
+#[derive(Debug, Clone, Copy)]
+pub enum PrimTy {
+    Never,
+    Unit,
+    Bool,
+    Char,
+    String,
+    Int,
+    Float,
+}
